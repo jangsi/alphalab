6b8d03e809f94cfd0d95e5899b3d7af25a424df6
Ryland <rylandmathews1@gmail.com>
2021-09-08 11:48:15 -0400

    alpha asttronaut

--- src/pages/Dashboard-blog/card-welcome.js
+++ src/pages/Dashboard-blog/card-welcome.js
@@ -14,7 +14,7 @@ class CardWelcome extends Component {
               <Col lg="9" sm="8">
                 <div className="p-4">
                   <h5 className="text-primary">Terra Commission APRs</h5>
-                  <p>Alpha Astronaut</p>
+                  <p>Welcome Alpha Astronaut</p>
 
                   <div className="text-muted">
                     <p className="mb-1">
--- src/pages/Dashboard-crypto/card-welcome.js
+++ src/pages/Dashboard-crypto/card-welcome.js
@@ -14,7 +14,7 @@ class CardWelcome extends Component {
               <Col lg="9" sm="8">
                 <div className="p-4">
                   <h5 className="text-primary">Alpha DeFi Spread Tracker</h5>
-                  <p>Alpha Astronaut</p>
+                  <p>Welcome Alpha Astronaut</p>
 
                   <div className="text-muted">
                     <p className="mb-1">
