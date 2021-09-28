f8c6775cca9a229856d895673b50e6826050b646
Ryland <rylandmathews1@gmail.com>
2021-09-28 10:35:31 -0400

    anchor graphic

--- src/pages/Dashboard-Anchor/card-welcome.js
+++ src/pages/Dashboard-Anchor/card-welcome.js
@@ -34,7 +34,7 @@ class CardWelcome extends Component {
               </Col>
               <Col lg="3" sm="4" className="align-self-center">
                 <div>
-                  <img src="//alphadefi.fund/wp-content/uploads/2021/08/687474703a2f2f74657272612e6d6f6e65792f6c6f676f732f74657272615f6c6f676f2e737667.svg" alt="" className="img-fluid d-block" />
+                  <img src="//pbs.twimg.com/media/E1J63l8VUAML6XD?format=png&name=4096x4096" alt="" className="img-fluid d-block" />
                 </div>
               </Col>
             </Row>
